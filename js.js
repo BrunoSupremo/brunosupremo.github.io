899015749d9b3667024888e2b27ac4b299b66470
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2021-06-10 10:48:25 -0300

    multiplos temas

--- js.js
+++ js.js
@@ -1,15 +1,50 @@
-const element_list = document.querySelectorAll(":is(a,p,button,h1,h2,img)");
+function change_theme(button, theme) {
+	document.querySelector("#options .current").classList.remove("current");
+	button.classList.add("current");
+
+	document.documentElement.className = "";
+	document.documentElement.classList.add(theme, "transitioning");
+	setTimeout(function (){
+		document.documentElement.classList.remove("transitioning")
+	}, 3000);
+
+	localStorage.setItem("theme", theme);
+}
+
+const loop_timer = setInterval(one_second_loop, 1010);
+function one_second_loop() {
+	//conditionals just to avoid running unnecessary lag
+	if(document.documentElement.className == "cyber"){
+		glitch();
+	}
+	if(document.documentElement.className == "slime"){
+		slime_borders();
+	}
+}
+
+const glitchables = document.querySelectorAll(":is(a,p,button,h1,h2,label,input)");
 let glitched_element = null;
 
 function glitch() {
-	glitched_element = element_list[Math.floor(Math.random()*element_list.length)];
+	glitched_element = glitchables[Math.floor(Math.random()*glitchables.length)];
 	glitched_element.classList.add("glitch");
 
-	setTimeout(unglitch, 20);
-	setTimeout(glitch, 1000);
-}
-function unglitch() {
-	glitched_element.classList.remove("glitch");
+	setTimeout(function(){
+		glitched_element.classList.remove("glitch");
+	}, 20);
 }
 
-glitch();
+function apply_random_border_classes() {
+	const random_border_elements = document.querySelectorAll(":is(section, img, button, input, textarea)");
+	for (let i = 0; i < random_border_elements.length; i++) {
+		random_border_elements[i].classList.add("random_slime_"+(i%8 +1));
+	}
+}
+apply_random_border_classes();
+
+function slime_borders() {
+	for (let i = 0; i < 8; i++) {
+		document.documentElement.style.setProperty(
+			'--random_radius_'+Math.floor(Math.random()*8+1), Math.random()*5+1 + "em");
+	}
+}
