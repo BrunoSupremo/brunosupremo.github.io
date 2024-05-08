4343a2fdfd84644170cd373d1df611fa6df7e88a
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2024-05-08 18:21:35 -0300

    There is such a thing as "font too big"...
    Removed lagged transitions
    More tight and centralized positions
    keyboard navigation styled too
    favicon added
    windows 95 theme added
    better skeuomorphism for construction theme
    improved visual and animation of slime theme

--- js.js
+++ js.js
@@ -6,11 +6,7 @@ function change_theme(button) {
 	document.querySelector("#options .current").classList.remove("current");
 	button.classList.add("current");
 
-	document.documentElement.className = "";
-	document.documentElement.classList.add(theme, "transitioning");
-	setTimeout(function (){
-		document.documentElement.classList.remove("transitioning")
-	}, 2000);
+	document.documentElement.className = theme;
 
 	localStorage.setItem("theme", theme);
 }
@@ -43,8 +39,12 @@ function glitch() {
 }
 
 function apply_random_border_classes() {
-	// so two neighbor elements do not have visually the same corner with same shape
-	const random_border_elements = document.querySelectorAll(":is(section, img, button, input, textarea)");
+	// so adjacent neighbors have different corners settings, avoiding synced animations
+	const random_border_sections = document.querySelectorAll("section");
+	for (let i = 0; i < random_border_sections.length; i++) {
+		random_border_sections[i].classList.add("random_slime_"+(i%8 +1));
+	}
+	const random_border_elements = document.querySelectorAll(":is(img, button, input, textarea)");
 	for (let i = 0; i < random_border_elements.length; i++) {
 		random_border_elements[i].classList.add("random_slime_"+(i%8 +1));
 	}
@@ -53,5 +53,5 @@ apply_random_border_classes();
 
 function slime_borders() {
 	document.documentElement.style.setProperty(
-		'--random_radius_'+Math.floor(Math.random()*8+1), Math.floor(Math.random()*5+2) + "em");
+		'--random_radius_'+Math.floor(Math.random()*8+1), Math.floor(Math.random()*6+1) + "em");
 }
