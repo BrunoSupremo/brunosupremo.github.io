fe73aef4cc738e0f83ab4bfbf8d148b4efb38b1d
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2025-12-06 08:38:44 -0300

    Noir theme
    Fixed links being lists
    Improved portfolio/default
    Cleaned slime code

--- js.js
+++ js.js
@@ -9,22 +9,12 @@ function change_theme(button) {
 	document.documentElement.className = theme;
 
 	localStorage.setItem("theme", theme);
-}
 
-const loop_timer = setInterval(one_second_loop, 1010);
-function one_second_loop() {
-	//conditionals to avoid running unnecessary lag
-	if(document.documentElement.className == "cyber"){
-		glitch();
-	}
-	if(document.documentElement.className == "slime"){
-		slime_borders();
-	}
+	theme_based_actions()
 }
 
 const glitchables = document.querySelectorAll(":is(span,img,a,p,button,h1,h2,label,input)");
 let glitched_element = glitchables[0];
-
 function glitch() {
 	if(Math.random()>.5){
 		return
@@ -38,20 +28,63 @@ function glitch() {
 	}, 550);
 }
 
-function apply_random_border_classes() {
-	// so adjacent neighbors have different corners settings, avoiding synced animations
-	const random_border_sections = document.querySelectorAll("section");
-	for (let i = 0; i < random_border_sections.length; i++) {
-		random_border_sections[i].classList.add("random_slime_"+(i%8 +1));
-	}
-	const random_border_elements = document.querySelectorAll(":is(img, button, input, textarea)");
-	for (let i = 0; i < random_border_elements.length; i++) {
-		random_border_elements[i].classList.add("random_slime_"+(i%8 +1));
-	}
-}
-apply_random_border_classes();
-
+const slime_elements = Array.from(document.querySelectorAll('section, img, button, input, textarea'));
 function slime_borders() {
-	document.documentElement.style.setProperty(
-		'--random_radius_'+Math.floor(Math.random()*8+1), Math.floor(Math.random()*6+1) + "em");
+	const random_half = slime_elements
+	.sort(() => Math.random() - 0.5)
+	.slice(0, Math.floor(slime_elements.length / 2));
+
+	random_half.forEach(element => {
+		element.style.setProperty(
+			'--variable_radius_'+Math.floor(Math.random()*4+1),
+			(Math.random()*6+1) + "em");
+	});
 }
+
+let noir_light = {x: 100, y: 200}
+function noir_mouse_events(event) {
+	noir_light.x  = event ? event.clientX : noir_light.x;
+	noir_light.y = event ? event.clientY : noir_light.y;
+
+	document.documentElement.style.setProperty('--light_x', noir_light.x+window.scrollX + "px");
+	document.documentElement.style.setProperty('--light_y', noir_light.y+window.scrollY + "px");
+
+	document.querySelectorAll("section,img,button,input,textarea,h1,h2,p").forEach(element => {
+		let rect = element.getBoundingClientRect()
+		let distanceX = noir_light.x - (rect.left + rect.width/2);
+		let distanceY = noir_light.y - (rect.top + rect.height/2);
+
+		element.style.setProperty('--distanceX_from_mouse', distanceX + "px");
+		element.style.setProperty('--distanceY_from_mouse', distanceY + "px");
+	});
+}
+
+let loop_timer = null
+function theme_based_actions() {
+	let theme = localStorage.getItem("theme")
+	clearInterval(loop_timer)
+
+	if(theme == "cyber"){
+		loop_timer = setInterval(glitch, 1010);
+	}
+	if(theme == "slime"){
+		loop_timer = setInterval(slime_borders, 1010);
+	}
+	if(theme == "noir"){
+		document.addEventListener("mousemove", noir_mouse_events)
+
+		function update_without_mouse() {
+			noir_mouse_events()
+		}
+
+		window.addEventListener("scroll", update_without_mouse);
+		window.addEventListener("resize", update_without_mouse);
+
+		update_without_mouse();
+	}else{
+		document.removeEventListener("mousemove", noir_mouse_events);
+		window.removeEventListener("scroll", update_without_mouse);
+		window.removeEventListener("resize", update_without_mouse);
+	}
+}
+theme_based_actions()
